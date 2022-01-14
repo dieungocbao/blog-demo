ca568368f299ed89624b0ad9d9d496edd13113ef
dieungocbao <dieungocbao@gmail.com>
2022-01-14 12:08:48 +0700

    setup express server

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+
+// middlewares
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+// Routes
+app.get('/', (req,res) => {
+  res.json({msg: "Hello world"})
+})
+
+// server listening
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Serving is running on port ', PORT))
